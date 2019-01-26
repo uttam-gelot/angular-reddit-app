399de69103552e66ac2202ff8c7f86c1e1edf731
uttam_mali <uttam@localhost.localdomain>
2019-01-26 16:54:50 +0530

    heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reddit';
+export class AppComponent 
+{
+    articles: Article[];
+    constructor()
+    {
+        this.articles = [];
+    }
+    addArticle(title: HTMLInputElement, link: HTMLInputElement)
+    {
+        if(title.value !== '' && link.value !== '')
+        {
+            this.articles.push(new Article(title.value, link.value, 0));
+        }
+        title.value = '';
+        link.value = '';
+    }
+    sortedArticles(): Article[] 
+    {
+        return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+    }
 }
